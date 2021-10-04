3feb88589c36389d8978d23849424d424e01ff5a
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-10-04 22:27:02 +0500

    Create a New React App

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import React from 'react';
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
